d4d420359a30ed34014081b2455f9a97269e318c
Kenneth Long <kdlong@hep.wisc.edu>
2020-11-17 15:49:54 +0100

    Add script to print fragment

--- fragmentFromDAS.py
+++ fragmentFromDAS.py
@@ -1,2 +1,18 @@
+#!/usr/bin/env python3
+from InputTools import UserInput 
 
+parser = UserInput.commonParser()
+parser.add_argument("--powheg", action='store_true', help="Is a POWHEG sample")
+parser.add_argument("-o", "--outfolder", type=str, default=".", help="Folder to store output (default cwd)")
+args = parser.parse_args()
+
+edmFile = args.edmfile if args.edmfile else UserInput.firstFileFromDAS(args.das)
+prov = UserInput.edmProv(edmFile, "Pythia8HadronizerFilter")
+
+foundGenerator = False
+for line in prov.split("\n"):
+    if "Pythia8HadronizerFilter" in line:
+        foundGenerator = True
+    if foundGenerator:
+        print line.strip()
 
